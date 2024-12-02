306c6c951f7c1e0a55b2666ae4c7331e7f708136
FASami <foysal.developer@gmail.com>
2024-12-02 17:17:11 +0600

    feat: add TypeScript definitions for authentication forms
    - Introduce new TypeScript definitions for authentication form fields, actions, and responses.
    - Define interfaces for `AuthFormField`, `AuthPageIdentifier`, and `AuthResponse` to standardize form handling.
    - Create `AuthFormProps` type to specify required properties for authentication form components.

---
+++ src/types/auth-form.d.ts
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react'
+import * as z from 'zod'
+
+export interface AuthFormField {
+  type: 'text' | 'password' | 'phone' | 'email'
+  name: string
+  label: string
+  placeholder: string
+  icon?: JSX.Element
+  initialValue: string
+  hidden?: boolean
+}
+
+export type AuthPageIdentifier =
+  | 'login'
+  | 'register'
+  | 'forgotPassword'
+  | 'setPassword'
+  | 'verify'
+  | 'changePassword'
+
+export type AuthResponse = {
+  success: boolean
+  error?: string
+  message?: string
+}
+
+export type AuthAction<T extends z.ZodType<unknown>> = (
+  values: z.infer<T>,
+  options: { callbackUrl: string; token: string; redirect?: boolean }
+) => Promise<AuthResponse | void>
+
+export interface AuthFormProps<TSchema extends z.ZodType<unknown>> {
+  onFormSubmit: AuthAction<TSchema>
+  identifier: AuthPageIdentifier
+  fields: AuthFormField[]
+  schema: TSchema
+  resendButton?: ReactNode
+}
