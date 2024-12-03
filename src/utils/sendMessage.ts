b81405491ba11f85acf9b39beecf100d232eadf0
FASami <foysal.developer@gmail.com>
2024-12-03 18:30:21 +0600

    feat: add sendMessage utility for SMS integration

---
+++ src/utils/sendMessage.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const sendMessage = async ({
+  phone,
+  message
+}: {
+  phone: string
+  message: string
+}) => {
+  console.log('Sending message to', phone)
+  const formattedPhoneNumber = phone.replaceAll(' ', '').replaceAll('+', '')
+  const url = `http://portal.khudebarta.com:3775/sendtext?apikey=${process.env.KHUDEBARTA_API_KEY}&secretkey=${process.env.KHUDEBARTA_API_SECRET}&callerID=${process.env.KHUDEBARTA_CALLER_ID}&toUser=${formattedPhoneNumber}&messageContent=${message}`
+  console.log('URL', url)
+  try {
+    const { data } = await axios.get(url)
+    if (data.TEXT === 'ACCEPTD') {
+      console.log('Send message')
+    }
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to send sms')
+  }
+}
+export { sendMessage }
