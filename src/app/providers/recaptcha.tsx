d88e2ba212735049a94a22f92ee5622f32cbf5b4
FASami <foysal.developer@gmail.com>
2024-12-03 18:36:31 +0600

    refactor: remove console log for reCAPTCHA key in provider component

--- src/app/providers/recaptcha.tsx
+++ src/app/providers/recaptcha.tsx
@@ -4,7 +4,6 @@ import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 
 const ReCaptchaProvider = ({ children }: { children: React.ReactNode }) => {
   const key: string | undefined = process?.env?.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
-  console.log('[RECAPTCHA KEY]', key)
   return (
     <GoogleReCaptchaProvider reCaptchaKey={key ?? 'NOT DEFINED'}>
       {children}
