4b6e28199c47b6cf58267dbd5e3525e666210710
FASami <foysal.developer@gmail.com>
2024-12-03 15:30:18 +0600

    feat: enhance MUI theme with custom menu styles

--- src/theme.ts
+++ src/theme.ts
@@ -9,6 +9,25 @@ const theme = createTheme({
     primary: {
       main: '#ef4444'
     }
+  },
+  components: {
+    MuiMenu: {
+      styleOverrides: {
+        paper: {
+          boxShadow:
+            '0 3px 5px -2px rgba(239, 68, 68, 0.1), 0 2px 3px -1px rgba(239, 68, 68, 0.06)'
+        }
+      }
+    },
+    MuiMenuItem: {
+      styleOverrides: {
+        root: {
+          '&:hover': {
+            backgroundColor: '#f3f4f6'
+          }
+        }
+      }
+    }
   }
 })
 
