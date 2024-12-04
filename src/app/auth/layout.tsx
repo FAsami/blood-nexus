bbcc043d93ce0434fbb1b6968a02714e18ce26f9
FASami <foysal.developer@gmail.com>
2024-12-05 00:56:27 +0600

    tweak: decresed  auth layout margin

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = async ({
   children: React.ReactNode
 }>) => {
   return (
-    <div className="max-w-2xl mx-auto mt-28">
+    <div className="max-w-2xl mx-auto mt-16">
       <div>{children}</div>
       <div className="text-xs text-center text-neutral-400 py-4 max-w-sm mx-auto">
         This site is protected by reCAPTCHA and the Google{' '}
