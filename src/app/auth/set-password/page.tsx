6fc733fa8033e540e7eb5e6de72158b5b726e64c
FASami <foysal.developer@gmail.com>
2024-12-04 16:28:18 +0600

    feat: added set password page

--- src/app/auth/set-password/page.tsx
+++ src/app/auth/set-password/page.tsx
@@ -1,5 +1,19 @@
-const SetPasswordPage = () => {
-  return <div>SetPasswordPage</div>
+import { auth } from '@/auth'
+import { SetPasswordForm } from './SetPasswordForm'
+import { redirect } from 'next/navigation'
+import { Suspense } from 'react'
+
+const SetPasswordPage = async () => {
+  const session = await auth()
+  if (!session?.user) {
+    redirect('/auth/login')
+  }
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SetPasswordForm />
+    </Suspense>
+  )
 }
 
 export default SetPasswordPage
